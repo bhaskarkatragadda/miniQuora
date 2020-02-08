5562f90ee123fc277d92b5d59134988bce85b57c
sanatansingh <sanatan0105@gmail.com>
2020-02-08 19:11:14 +0530

    Added Ejs

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ mongoose.Promise = global.Promise;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 
 app.use(express.json());
