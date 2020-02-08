d0a870b42d61c504c7891d6a39a4615c94d0bafd
Bhaskar <katragaddabhaskar1729@gmail.com>
2020-02-08 13:26:05 +0530

    Initilized express app

--- app.js
+++ app.js
@@ -1,64 +1,41 @@
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-mongoose.set('useCreateIndex', true);
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-//Database Connection
-mongoose.connect('mongodb+srv://bhaskar:bhaskar@rest-shop-covsp.mongodb.net/test?retryWrites=true&w=majority',{
-        useNewUrlParser:true,
-        useUnifiedTopology:true
-    }
-);
-mongoose.Promise = global.Promise;
-//log
-app.use(morgan('dev'));
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-//making file public
-app.use('/uploads', express.static('uploads'));
-//body-parser
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+var app = express();
 
-//Handling CORS
-app.use((req, res, next) =>{
-    res.header("Access-Control-Allow-Origin",'*');
-    res.header("Access-Control-Allow-Headers","Origin, X-Requested-with, Content-Type, Accept, Authorization");
-    if(req.method === 'OPTIONS')
-    {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
-    }
-    next();
-});
-//path for routes
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-//middleware for routes
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-
-
-
-// Handling Errors past the routes
-app.use((req, res, next) => {
-    const error = new Error('Not found');
-    error.status = 404;
-    next(error);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-
-app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error:{
-            message: error.message
-        }
-    });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
-
-
 
 module.exports = app;
-
