f04beb7e5dfea0793339e6f5748e044b74f71aaf
sanatansingh <sanatan0105@gmail.com>
2020-02-08 21:28:59 +0530

    Added Ejs

--- routes/details.js
+++ routes/details.js
@@ -2,13 +2,15 @@ const express = require('express');
 const router = express.Router();
 const User  =  require('../models/userModel');
 const Question  =  require('../models/questionModel');
+const Answer  =  require('../models/answerSchema');
 
 
 router.get('/:ID', async (req, res, next)=>{
     //to get all the questions
     let questionId = req.params.ID;
     let  question = await Question.findById(questionId);
-    res.render('question-details', {question: question});
+    let answers = await Answer.find({questionId: questionId}).sort({createdAt: -1});
+    res.render('question-details', {question: question, answers: answers});
 });
 
 
--- routes/home.js
+++ routes/home.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const User  =  require('../models/userModel');
-const Question  =  require('../models/questionModel');
-const Answer  =  require('../models/answerSchema');
+const User = require('../models/userModel');
+const Question = require('../models/questionModel');
+const Answer = require('../models/answerSchema');
 
 /* GET home page. */
-router.get('/', async (req, res, next)=>{
+router.get('/', async (req, res, next) => {
     //to get all the questions
-   let  questions = await Question.find().sort({createdAt: -1});
-   res.render('homepage', {questions: questions});
+    let questions = await Question.find().sort({createdAt: -1});
+    res.render('homepage', {questions: questions});
 });
 
-router.get('/questionSearch', function(req, res, next) {
+router.get('/questionSearch', function (req, res, next) {
 
 });
 
-router.post('/createQuestion', async function(req, res, next) {
-    try{
-        const userId = '5e3ec39f60ad34abfbdb2dac';
+router.post('/createQuestion', async function (req, res, next) {
+    try {
+        const userId = req.cookies._vt;
         let question = req.body.question;
         let tag = req.body.tag;
         const questions = new Question({
@@ -27,36 +27,34 @@ router.post('/createQuestion', async function(req, res, next) {
         });
         await questions.save();
         res.status(200).json({
-            message:'Question Uploaded'
+            message: 'Question Uploaded'
         });
-    }catch (e) {
+    } catch (e) {
         res.status(500).json({
-            message:'Error in Uploading in the Question'
+            message: 'Error in Uploading in the Question'
         });
         console.log(e);
     }
 });
 
-router.post('/uploadAnswer', async function(req, res, next){
-try{
-  let ans = req.body.answer;
-  let questionId = '5e3ed0ee7fb2014289159c99';
-  let userId = '5e3e93ae7f92d02831a59908';
-  const answer = new Answer({
-      questionId:questionId,
-      answer:ans,
-      userId:userId
-  });
-  await answer.save();
-  res.status(200).json({
-      message:'Answer Uploaded'
-  });
-}catch (e) {
-    console.log(e);
-res.status(500).json({
-    message:'Answer uploading Failed'
-});
-}
+router.post('/uploadAnswer', async function (req, res, next) {
+    try {
+        let ans = req.body.answer;
+        let questionId = req.body.questionId;
+        let userId = req.cookies._vt;
+        const answer = new Answer({
+            questionId: questionId,
+            answer: ans,
+            userId: userId
+        });
+        await answer.save();
+        res.redirect('/details/'+ questionId)
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            message: 'Answer uploading Failed'
+        });
+    }
 });
 
 
